a375bb187cf58d5976b5340846199b60c0c028eb
Angel Concha Layme <aconchal@unsa.edu.pe>
2023-09-20 19:51:49 -0500

    driver

---
+++ src/main/java/com/angel/hadoopsearch/HadoopSearchDriver.java
@@ -0,0 +1,26 @@
+package com.angel.hadoopsearch;
+
+import com.angel.hadoopsearch.mapper.TokenizerMapper;
+import com.angel.hadoopsearch.reducer.IntSumReducer;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+public class HadoopSearchDriver {
+
+    public static void main(String[] args) throws Exception {
+        Configuration conf = new Configuration();
+        Job job = Job.getInstance(conf, "hadoop search");
+        job.setJarByClass(HadoopSearchDriver.class);
+        job.setMapperClass(TokenizerMapper.class);
+        job.setReducerClass(IntSumReducer.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(Text.class);
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
+    }
+}
