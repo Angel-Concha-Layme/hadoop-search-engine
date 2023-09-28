0087f3bc48aa6cf85bbb33e60dec20bd7eff95b1
Angel Concha Layme <aconchal@unsa.edu.pe>
2023-09-28 09:05:22 -0500

    Update HadoopSearchDriver.java

--- src/main/java/com/angel/hadoopsearch/HadoopSearchDriver.java
+++ src/main/java/com/angel/hadoopsearch/HadoopSearchDriver.java
@@ -2,6 +2,8 @@ package com.angel.hadoopsearch;
 
 import com.angel.hadoopsearch.mapper.TokenizerMapper;
 import com.angel.hadoopsearch.reducer.IntSumReducer;
+import com.angel.hadoopsearch.reducer.InvertedIndexCombiner;
+
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.Text;
@@ -13,12 +15,14 @@ public class HadoopSearchDriver {
 
     public static void main(String[] args) throws Exception {
         Configuration conf = new Configuration();
-        conf.setBoolean("mapreduce.input.fileinputformat.input.dir.recursive", true); // Habilita el procesamiento
-                                                                                      // recursivo de directorios
+        conf.setBoolean("mapreduce.input.fileinputformat.input.dir.recursive", true);
+        conf.set("mapreduce.map.output.compress", "true");
+        conf.set("mapreduce.map.output.compress.codec", "org.apache.hadoop.io.compress.SnappyCodec");
 
-        Job job = Job.getInstance(conf, "hadoop search");
+        Job job = Job.getInstance(conf, "hadoop search optimized");
         job.setJarByClass(HadoopSearchDriver.class);
         job.setMapperClass(TokenizerMapper.class);
+        job.setCombinerClass(InvertedIndexCombiner.class);
         job.setReducerClass(IntSumReducer.class);
         job.setOutputKeyClass(Text.class);
         job.setOutputValueClass(Text.class);
