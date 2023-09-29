ee5c714099c310a58c64c5e69204e3db06aa862d
Angel Concha Layme <aconchal@unsa.edu.pe>
2023-09-28 23:23:23 -0500

    Update HadoopSearchDriver.java

--- src/main/java/com/angel/hadoopsearch/HadoopSearchDriver.java
+++ src/main/java/com/angel/hadoopsearch/HadoopSearchDriver.java
@@ -3,28 +3,22 @@ package com.angel.hadoopsearch;
 import com.angel.hadoopsearch.mapper.TokenizerMapper;
 import com.angel.hadoopsearch.reducer.FileListReducer;
 
-import java.net.URI;
-
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
+
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 public class HadoopSearchDriver {
-
     public static void main(String[] args) throws Exception {
         Configuration conf = new Configuration();
-        // Agrega la ruta del archivo cacheado en tu configuración
-
         conf.setBoolean("mapreduce.input.fileinputformat.input.dir.recursive", true);
         conf.set("mapreduce.map.output.compress", "true");
         conf.set("mapreduce.map.output.compress.codec", "org.apache.hadoop.io.compress.SnappyCodec");
 
         Job job = Job.getInstance(conf, "hadoop search optimized");
-        // Agrega la ruta del archivo cacheado en tu configuración
-        job.addCacheFile(new URI("/user/angel/DirIdMapping/part-r-00000"));
 
         job.setJarByClass(HadoopSearchDriver.class);
         job.setMapperClass(TokenizerMapper.class);
@@ -32,8 +26,11 @@ public class HadoopSearchDriver {
         job.setReducerClass(FileListReducer.class);
         job.setOutputKeyClass(Text.class);
         job.setOutputValueClass(Text.class);
-        FileInputFormat.addInputPath(job, new Path(args[0]));
+
+        FileInputFormat.addInputPaths(job, args[0] + "," + args[2]);
+
         FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
         System.exit(job.waitForCompletion(true) ? 0 : 1);
     }
 }
